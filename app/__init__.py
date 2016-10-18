b9ac225a6c59fe03bda4f33d9ffc1a3c3bb999bb
Janne Mäyrä <jaeeolma@student.jyu.fi>
2016-10-18 12:06:58 +0300

    __init__.py käynnistää jo sivun joka hakee videoita

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,31 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
+from yle2 import get_video_list
+from yle2 import get_video_url
 import os
 import csv
+import random
 
 
 app = Flask(__name__)
 
-urltesti = 'moi'
+
 
 
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
+    try:
+        year = request.form['year']
+    except:
+        year = '1993'
+    tulokset = get_video_list(year)
+    if len(tulokset) == 0:
+        tulokset = get_video_list('1992')
+    mid = random.choice(tulokset)
+    url = get_video_url(mid)
     return render_template('base.html',
-                           url=urltesti)
+                           url=url,
+                           mid=mid)
 
 
 if __name__ == '__main__':
