90990d19d1d33af465043419fa84deb14bc78f6e
unknown <vitutera@HATTIVAT.ad.jyu.fi>
2016-11-11 14:43:17 +0200

    Nyt pitäis toimia...

--- app/__init__.py
+++ app/__init__.py
@@ -68,8 +68,8 @@ def hello_world():
 
     return render_template('base.html',
                            postimerkki_url=postimerkki_url,
-                           url=areena_url,
-                           mid=areena_mid,
+                           areena_url=url,
+                           areena_mid=mid,
                            finna_kuva=finna_kuva,
                            finna_record = finna_record,
                            finna_title = finna_title,
