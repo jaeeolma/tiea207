636723004d265cb0b58a087061055a5269c35484
rijokukk <rijokukk@student.jyu.fi>
2016-11-22 13:05:15 +0200

    Init jaettu omiin templateihin
    Init jaettu omiin templateihin

--- app/__init__.py
+++ app/__init__.py
@@ -34,41 +34,13 @@ app.static_folder = 'static'
 #app.register_blueprint(ck, url_prefix='/ck')
 #app.jinja_env.add_extension("chartkick.ext.charts")
 
-@app.route('/', methods=['POST', 'GET'])
-def hello_world():
+@app.route('/faktat', methods=['POST', 'GET'])
+def faktat():
     try:
         year = request.form['year']
     except:
         year = '1962'
 
-    tulokset = get_video_list(year)
-    if len(tulokset) == 0:
-        mid = ''
-        url = ''
-    else:
-        mid = random.choice(tulokset)
-        url = get_video_url(mid)
-
-    postimerkit = merkin_url(year)
-    postimerkki_urlit = []
-    postimerkki_tiedot = []
-    if len(postimerkit) == 0:
-        postimerkki_urlit.append('')
-        postimerkki_url = ''
-    else:
-        for x in range(0, 4):
-            postimerkki_url = random.choice(postimerkit)
-            if (postimerkki_url not in postimerkki_urlit):
-                postimerkki_urlit.append(postimerkki_url)
-                url_tiedot = merkin_tiedot(postimerkki_url)
-                postimerkki_tiedot = postimerkki_tiedot + url_tiedot
-        
-    finnaresult = search_finna(year)
-    finna_kuva = FINNA_IMAGE_URL + finnaresult['image']
-    finna_record = FINNA_RECORD_URL + urllib.quote(finnaresult['id'])
-
-    finna_title = finnaresult['title']
-    finna_source = finnaresult['building']
     chartID = 'vaesto'
     chart_type = 'bar'
     chart = {"renderTo": chartID, "type": chart_type}
@@ -101,13 +73,6 @@ def hello_world():
         
     
     return render_template('base.html',
-                           postimerkki_url=postimerkki_url,
-                           areena_url=url,
-                           areena_mid=mid,
-                           finna_kuva=finna_kuva,
-                           finna_record = finna_record,
-                           finna_title = finna_title,
-                           finna_source = finna_source,
                            year=year,
                            chartID = chartID,
                            chart = chart,
@@ -117,12 +82,69 @@ def hello_world():
                            yAxis = yAxis,
                            plotOptions = plotOptions,
                            tooltip = tooltip,
-                           postimerkki_urlit = postimerkki_urlit,
-                           postimerkki_tiedot = postimerkki_tiedot,
                            presidentin_kuva=presidentin_kuva,
                            presidentin_nimi=presidentin_nimi,
                            paaministeri_url=paaministeri_url,
                            paaministeri_nimi=paaministeri_nimi)
+
+@app.route('/kuvat', methods=['POST', 'GET'])
+def kuvat():
+    try:
+        year = request.form['year']
+    except:
+        year = '1962'
+
+    finnaresult = search_finna(year)
+    finna_kuva = FINNA_IMAGE_URL + finnaresult['image']
+    finna_record = FINNA_RECORD_URL + urllib.quote(finnaresult['id'])
+    finna_title = finnaresult['title']
+    finna_source = finnaresult['building']
+
+    postimerkit = merkin_url(year)
+    postimerkki_urlit = []
+    postimerkki_tiedot = []
+    if len(postimerkit) == 0:
+        postimerkki_urlit.append('')
+    else:
+       # for x in range(0, 14):
+         for x in range(len(postimerkit)):
+           # postimerkki_url = random.choice(postimerkit)
+           # if (postimerkki_url not in postimerkki_urlit):
+           #     postimerkki_urlit.append(postimerkki_url)
+                url_tiedot = merkin_tiedot(postimerkit[x])
+                postimerkki_tiedot = postimerkki_tiedot + url_tiedot
+
+
+    return render_template('kuvat.html',
+                           #postimerkki_urlit=postimerkki_urlit,
+                           postimerkit_urlit=postimerkit,
+                           postimerkki_tiedot=postimerkki_tiedot,
+                           finna_kuva=finna_kuva,
+                           finna_record=finna_record,
+                           finna_title=finna_title,
+                           finna_source=finna_source,
+                           year=year)
+
+@app.route('/videot', methods=['POST', 'GET'])
+def videot():
+    try:
+        year = request.form['year']
+    except:
+        year = '1962'
+
+    tulokset = get_video_list(year)
+    if len(tulokset) == 0:
+        mid = ''
+        url = ''
+    else:
+        mid = random.choice(tulokset)
+        url = get_video_url(mid)
+
+
+    return render_template('videot.html',
+                           areena_url=url,
+                           areena_mid=mid,
+                           year=year)
 
 if __name__ == '__main__':
     app.run()
