2572e45cdba97b7778af6f3e42e3cecd3c2d1d47
Viivi Teräväinen <vitutera@student.jyu.fi>
2016-12-05 22:07:14 +0200

    ulkoasua muokattu ja aloitusvuodeksi vaihdettu 1966

--- app/__init__.py
+++ app/__init__.py
@@ -22,7 +22,7 @@ sys.setdefaultencoding("utf-8")
 
 FINNA_RECORD_URL='https://www.finna.fi/Record/'
 FINNA_IMAGE_URL='https://api.finna.fi'
-VUOSI = 1962
+VUOSI = 1966
 
 AGE_GROUPS = ['85-', '80-84', '75-79', '70-74', '65-69', '60-64', '55-59', '50-54', '45-49', '40-44', '35-39', '30-34', '25-29', '20-24', '15-19', '10-14', '5-9', '0-4']
 
