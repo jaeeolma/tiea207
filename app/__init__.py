f0f0e0200627eed032a11c4b1c9cda013506d3cb
Janne Mäyrä <jaeeolma@student.jyu.fi>
2016-12-13 10:36:48 +0200

    ei kaadu kokonaan jos ei onnistukkaan hakemaan käynnistyessä elävän arkiston metadatoja

--- app/__init__.py
+++ app/__init__.py
@@ -37,15 +37,18 @@ app.static_folder = 'static'
 
 @app.before_first_request
 def update_files():
-    articles = 'http://elavaarkisto.kokeile.yle.fi/data/articles.csv'
-    mediaarticle = 'http://elavaarkisto.kokeile.yle.fi/data/media-article.csv'
-    media = 'http://elavaarkisto.kokeile.yle.fi/data/media.csv'
-    path = os.path.join(os.path.dirname(__file__), '../data/articles.csv')
-    urllib.urlretrieve(articles, path)
-    path = os.path.join(os.path.dirname(__file__), '../data/media-article.csv')
-    urllib.urlretrieve(mediaarticle, path)
-    path = os.path.join(os.path.dirname(__file__), '../data/media.csv')
-    urllib.urlretrieve(media, path)
+    try:
+        articles = 'http://elavaarkisto.kokeile.yle.fi/data/articles.csv'
+        mediaarticle = 'http://elavaarkisto.kokeile.yle.fi/data/media-article.csv'
+        media = 'http://elavaarkisto.kokeile.yle.fi/data/media.csv'
+        path = os.path.join(os.path.dirname(__file__), '../data/articles.csv')
+        urllib.urlretrieve(articles, path)
+        path = os.path.join(os.path.dirname(__file__), '../data/media-article.csv')
+        urllib.urlretrieve(mediaarticle, path)
+        path = os.path.join(os.path.dirname(__file__), '../data/media.csv')
+        urllib.urlretrieve(media, path)
+    except:
+        pass
 
 @app.route('/', methods=['POST', 'GET'])
 def faktat():
